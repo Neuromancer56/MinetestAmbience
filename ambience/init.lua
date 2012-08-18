04fa3ac937db3aec12e973782b5296babd26446f
Neuromancer <neuromancer56@gmail.com>
2012-08-17 21:03:08 -0500

    added owls wolves & increased crickets

--- ambience/init.lua
+++ ambience/init.lua
@@ -9,15 +9,27 @@ minetest.register_globalstep(function(time)
 		sound_playing = time
 		end
 
-		if sound_playing > 1 and time < 0.2 then
-		sound_playing = 0.2
+		if sound_playing > 1 and time < 0.05 then
+		sound_playing = 0.05
 		end
-	
+		
+			--random wolves & owls at night
+		if time > 0.8 or time < 0.2 then
+				if math.random(10000) >9997 then
+			 	minetest.sound_play("Wolves_Howling")
+			end		 	
+			if math.random(10000) >9997 then
+			 	minetest.sound_play("horned_owl")
+			end	
+		end	
 		if time > sound_playing then
 
 			if time > 0.8 or time < 0.2 then
-			sound_playing = time + 0.1
+			sound_playing = time + 0.07
 			minetest.sound_play("Crickets_At_NightCombo")
+	
+	 
+			
 			return true
 			end	
 
